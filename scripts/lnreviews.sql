02dfd8447f2d6138b6305fbfb1959d998bc3e1db
Anthony Cuña <127514363+thony-cna@users.noreply.github.com>
2025-11-06 21:55:55 -0300

    se quitan reviews de lnreviews a las cuales no se le puede asociar un libro

--- scripts/lnreviews.sql
+++ scripts/lnreviews.sql
@@ -30,3 +30,7 @@ SELECT
 FROM l1ratingsstage1 r
 JOIN l1usersstage1 u
   ON r.user_id = u.l1_user_id;
+
+--- Borrar reviews a las cuales no se le puede asociar un libro (no es un libro de l1, ni tampoco de l2)
+DELETE FROM lnreviews r
+WHERE NOT EXISTS (SELECT 1 FROM lnbooks b WHERE UPPER(b.identifier)=UPPER(r.isbn));
